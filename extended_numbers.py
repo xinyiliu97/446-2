990d781bf94c3d9dec0640c187761ab4caf1b71c
xinyiliu97 <liu.xinyi.self@gmail.com>
2022-01-09 19:35:54 -0600

    all pytests should past

--- extended_numbers.py
+++ extended_numbers.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numbers
+
 class C:
     
     def __init__(self, data):
@@ -10,3 +12,21 @@ class C:
         return np.allclose(self.data, other.data)
     def __add__(self, other):
         return C(self.data + other.data)
+    def __neg__(self):
+        return C(-self.data)
+    def __sub__(self, other):
+        return self + (-other)
+    def __mul__(self, other):
+        if isinstance(other, C):
+            return C((self.data[0] * other.data[0] - self.data[1]*other.data[1],
+                 self.data[0] * other.data[1] + self.data[1]*other.data[0]))
+        elif isinstance(other, numbers.Number):
+            return C(other * self.data)
+        else:
+            raise ValueError
+    def __rmul__(self, other):
+        if isinstance(other, numbers.Number):
+            return C(other * self.data)
+        else:
+            raise ValueError 
+        
